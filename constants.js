34d6c7dc2b03471844441969555f04db4f0d4545
coolguy284 <coolguy284v64@gmail.com>
2023-10-12 06:40:45 -0400

    change default time format back to 12hr
    was changed during testing and forgot to change back
    
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
 // constants to change program operation
 let LOG_DEBUG = false;
-let CLOCK_DRAW_MODE = '24 Hour'; // either "12 Hour" or "24 Hour" (fancy looking 24hr clock)
+let CLOCK_DRAW_MODE = '12 Hour'; // either "12 Hour" or "24 Hour" (fancy looking 24hr clock)
 let CLOCK_12HR_MOTIF = true; // whether sun / moon motif will show in center of clock
