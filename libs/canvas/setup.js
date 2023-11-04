b3d8be7ee835be7b13487347f126886aa4ca1f39
coolguy284 <coolguy284v64@gmail.com>
2023-11-04 03:55:27 -0400

    scale canvas properly on mobile
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- libs/canvas/setup.js
+++ libs/canvas/setup.js
@@ -2,8 +2,9 @@
 function resetCanvasSize() {
   let canvasStyle = getComputedStyle(canvas);
   
-  let canvasWidth = parseInt(canvasStyle.width);
-  let canvasHeight = parseInt(canvasStyle.height);
+  // scale canvas width and height by device pixel ratio to prevent highly grainly canvas on mobile
+  let canvasWidth = parseInt(canvasStyle.width) * window.devicePixelRatio;
+  let canvasHeight = parseInt(canvasStyle.height) * window.devicePixelRatio;
   
   canvas.width = canvasWidth;
   canvas.height = canvasHeight;
